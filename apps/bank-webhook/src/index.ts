0b8e00d2adc790e65e8c5872bd117d52d6e963cf
samrat53 <samrat05hccs@gmail.com>
2024-06-18 04:33:35 +0530

    updating the onramp table and balances table using the webhook, prtsma transactions

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,26 +1,46 @@
 import express from "express";
 import db from "@repo/db/client";
 
-const app=express();
-app.post("/hdfsWebHook",async(req,res)=>{
-    const paymentInformation={
-        token: req.body.token,
-        userId:req.body.user_identifier,
-        amount: req.body.amount
-    }
-    db.balance.update({
-        where:{
-            userId: paymentInformation.userId
+const app = express();
+app.post("/hdfsWebHook", async (req, res) => {
+  const paymentInformation = {
+    token: req.body.token,
+    userId: req.body.user_identifier,
+    amount: req.body.amount,
+  };
+
+  // updating the user balance in the app
+  try {
+    await db.$transaction([
+      db.balance.updateMany({
+        where: {
+          userId: Number(paymentInformation.userId),
         },
-        data:{
-            amount:{
-                increment:paymentInformation.amount
-            }
-        }
-    })
+        data: {
+          amount: {
+            increment: Number(paymentInformation.amount),
+          },
+        },
+      }),
+      db.onRampTransaction.updateMany({
+        where: {
+          token: paymentInformation.token,
+        },
+        data: {
+          status: "Success",
+        },
+      }),
+    ]);
 
-    // db.onRampTransaction.update({
+    res.status(200).json({
+      message: "captured",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(411).json({
+      message: "Error whike processing webhook",
+    });
+  }
+});
 
-    // })
-    // updating the user balance
-})
+app.listen(3000);
