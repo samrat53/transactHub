6038075a269329b43dc66cfd637a1bebdf7b1aea
samrat53 <samrat05hccs@gmail.com>
2024-06-19 19:45:06 +0530

    added transaction to processing OnRamp

--- packages/db/prisma/seed.ts
+++ packages/db/prisma/seed.ts
@@ -1,51 +1,38 @@
 import { PrismaClient } from '@prisma/client'
-import bcrypt from "bcrypt";
 const prisma = new PrismaClient()
 
 async function main() {
   const alice = await prisma.user.upsert({
-    where: { number: '1111111111' },
+    where: { number: '9999999999' },
     update: {},
     create: {
-      number: '1111111111',
-      password: await bcrypt.hash('alice', 10),
+      number: '9999999999',
+      password: 'alice',
       name: 'alice',
-      Balance: {
-        create: {
-            amount: 20000,
-            locked: 0
-        }
-      },
       OnRampTransaction: {
         create: {
           startTime: new Date(),
           status: "Success",
           amount: 20000,
-          token: "token__1",
+          token: "122",
           provider: "HDFC Bank",
         },
       },
     },
   })
   const bob = await prisma.user.upsert({
-    where: { number: '2222222222' },
+    where: { number: '9999999998' },
     update: {},
     create: {
-      number: '2222222222',
-      password: await bcrypt.hash('bob', 10),
+      number: '9999999998',
+      password: 'bob',
       name: 'bob',
-      Balance: {
-        create: {
-            amount: 2000,
-            locked: 0
-        }
-      },
       OnRampTransaction: {
         create: {
           startTime: new Date(),
           status: "Failure",
           amount: 2000,
-          token: "token__2",
+          token: "123",
           provider: "HDFC Bank",
         },
       },
